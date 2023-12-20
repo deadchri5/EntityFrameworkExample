68bc3f5067009e00ef703bd01acbabab5d955584
Christian Ochoa <a20110469@live.ceti.mx>
2023-12-20 10:51:09 -0600

    metodo eliminar y actualizar

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace efejemplo.Models;
 
@@ -25,6 +26,7 @@ public class Task
     public virtual Category Category { get; set; }
 
     // [NotMapped]
+    [JsonIgnore]
     public string Resumen { get; set; }
 
 }
--- Program.cs
+++ Program.cs
@@ -19,10 +19,50 @@ app.MapGet("/dbConnection", async ([FromServices] TasksContext dbContext) => {
     return Results.Ok("Base de datos en memoria " + dbContext.Database.IsInMemory());
 });
 
-app.MapGet("/api/getTasks", async ([FromServices] TasksContext dbContext) => {
+app.MapGet("/api/getTasks", async (TasksContext dbContext) => {
     // return Results.Ok(dbContext.Tasks.Where(p => p.TaskProperty == efejemplo.Models.Properties.LOW));
     var tasks = await dbContext.Tasks.Include(p => p.Category).ToListAsync();
     return Results.Ok(tasks);
+});
+
+app.MapPost("/api/postTask", async (TasksContext dbContext, efejemplo.Models.Task task) => {
+
+    task.TaskId = Guid.NewGuid();
+    task.DateCreation = DateTime.Now;
+
+    await dbContext.AddAsync(task);
+
+    await dbContext.SaveChangesAsync();
+
+    return Results.Ok();
+});
+
+app.MapPut("/api/editTask/{id}", async (TasksContext dbContext, efejemplo.Models.Task task, [FromRoute] Guid id) => {
+    
+    var findTask = dbContext.Tasks.Find(id);
+
+    if (findTask != null) {
+        findTask.Category = task.Category;
+        findTask.Title = task.Title;
+        findTask.Description = task.Description;
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
+app.MapDelete("/api/deleteTask/{id}", async (TasksContext dbContext, [FromRoute] Guid id) => {
+    
+    var findTask = dbContext.Tasks.Find(id);
+
+    if (findTask != null) {
+        dbContext.Remove(findTask);
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
 });
 
 app.Run();
