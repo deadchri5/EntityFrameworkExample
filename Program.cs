914248ffde60c525a39ec695c53d2d60525aaa8e
Christian Ochoa <a20110469@live.ceti.mx>
2023-12-19 22:49:50 -0600

    Se agregan servicios

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
+using efejemplo;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// add memory database service to builder
+// builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TaskDB"));
+// add sql server service to builder
+builder.Services.AddSqlServer<TasksContext>(builder.Configuration.GetSection("Server")["ConnectionString"]);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// new endpoint
+app.MapGet("/dbConnection", async ([FromServices] TasksContext dbContext) => {
+    dbContext.Database.EnsureCreated();
+    return Results.Ok("Base de datos en memoria " + dbContext.Database.IsInMemory());
+});
 
 app.Run();
